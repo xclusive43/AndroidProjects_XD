77b341b804281053e9d839eb5c4d03bca7222333
Ajay Prajapati <emocatc@gmail.com>
2022-05-03 20:42:24 +0530

    removed bca project

--- TourismTradesBCA/admin/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","tourismtrades") or die(mysql_error());
-
-?>
--- TourismTradesBCA/login/bottom.php
+++
@@ -1,8 +0,0 @@
- 
- 
-<div class="copyright text-center">
-    
-<p style="padding-right:100px"><b> Desinged By: RGU BCA students</b></p>
-</div>
- 
-  
